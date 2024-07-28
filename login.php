ee4d36c69d3eb872494de8c7722c89bde9a1c056
Sebastián Segura <ssegura30126@ufide.ac.cr>
2024-07-28 12:00:42 -0600

    Formulario Design
    Diseño del formulario actualizado y documentado, listo para implementar especificaciones en funcionalidad.

--- login.php
+++ login.php
@@ -11,35 +11,60 @@ if ($conn->connect_error) {
     die("Error de Conexión");
 }
 echo "<script>console.log('"."Conexión exitosa"."');</script>";
+// En la consola de nuestro se imprimirá este mensaje si la conexión al usuario XAMPP y la BD fueron exitosos.
 ?>
 
 <!DOCTYPE html>
+
 <html lang="esn">
+
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form | CodingLab</title> 
+    <title>Reserva Práctica 3</title> 
     <link rel="stylesheet" href="estilos.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <!-- la librería font-awesome nos da los iconos para los cuadros de texto, una forma de hacer un diseño más limpio y legible -->
   </head>
+
   <body>
     <div class="container">
       <div class="wrapper">
-        <div class="title"><span>Login Form</span></div>
+        <div class="title"><span>Realiza tu Reserva</span></div>
         <form action="#">
-          <div class="row">
+
+          <div class="row"> <!-- input de tipo texto en el que el cliente podrá ingresar su nombre. -->
             <i class="fas fa-user"></i>
-            <input type="text" placeholder="Email or Phone" required>
+            <input type="text" placeholder="Nombre Completo" required>
           </div>
-          <div class="row">
+
+          <div class="row"> <!-- input de tipo password en el que el cliente podrá ingresar su contraseña oculta. -->
             <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Password" required>
+            <input type="password" placeholder="Contraseña" required>
           </div>
-          <div class="pass"><a href="#">Forgot password?</a></div>
+
+          <div class="row">
+            <i class="fas fa-at"></i> <!-- input de tipo email en el que el cliente podrá ingresar su correo.
+                                           El programa lo obliga a utilizar @ para guardar los datos. -->
+            <input type="email" placeholder="Correo Electrónico" required>
+          </div>
+
+          <div class="row">
+            <i class="far fa-calendar-alt"></i> <!-- input de tipo texto, una vez se interactúe con él, se revela que es
+                                                     un tipo date enmascarado, para que el cliente ingrese la fecha de su "cita" -->
+            <input type="text" placeholder="Fecha para la Cita" onfocus="this.type='date'" onblur="this.type='text'" required>
+          </div>
+
+          <div class="row">
+            <i class="fas fa-comment-dots"></i> <!-- input de tipo texto en el que el cliente PUEDE dar un mensaje que acompañe
+                                                     a la reserva. No es obligatorio así que no lleva REQUIERED -->  
+            <input type="text" placeholder="Mensaje Adicional">
+          </div>
+
           <div class="row button">
-            <input type="submit" value="Login">
+            <input type="submit" value="Reservar">
           </div>
-          <div class="signup-link">Not a member? <a href="#">Signup now</a></div>
+          
         </form>
       </div>
     </div>
